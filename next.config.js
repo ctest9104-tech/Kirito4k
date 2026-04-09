eb5392803a2b27fab3895aa3d1e2bb4e170ee008
ctest9104-tech <ctest9104@gmail.com>
2026-04-09 01:24:50 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.vsembed.ru',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
