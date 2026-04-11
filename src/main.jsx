c478f4668a7fd1f2ffdd3543bd4fd9fedb842e74
ctest9104-tech <ctest9104@gmail.com>
2026-04-10 23:33:23 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
-import { Buffer } from 'buffer';
-import process from 'process';
-
-window.Buffer = Buffer;
-window.process = process;
-window.global = window;
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Using the exact name you confirmed is on GitHub
 import App from './App.jsx'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
