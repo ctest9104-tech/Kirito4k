e85445977813057b0cda20c9a741460ddace0b65
ctest9104-tech <ctest9104@gmail.com>
2026-04-10 22:43:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './',
   define: {
-    global: 'window', // Fixes "global is not defined" error
+    global: 'window',
   },
   resolve: {
     alias: {
+      // This line is the fix for the DHT "Client" error:
+      'bittorrent-dht': 'identity-obj-proxy', 
       path: 'path-browserify',
       crypto: 'crypto-browserify',
       stream: 'stream-browserify',
