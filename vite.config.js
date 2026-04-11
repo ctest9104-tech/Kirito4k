4a6adddac4abb1f59c36ad883af9406920ebe9ea
ctest9104-tech <ctest9104@gmail.com>
2026-04-10 21:44:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    // WebTorrent and some other P2P libs expect 'global' to exist
     global: 'window',
   },
   resolve: {
     alias: {
-      // This tells Vite to use the 'events' package you added to package.json
       events: 'events',
+      path: 'path-browserify',
     },
   },
   build: {
     outDir: 'dist',
-    // This helps handle large libraries like WebTorrent
     commonjsOptions: {
       transformMixedEsModules: true,
     },
